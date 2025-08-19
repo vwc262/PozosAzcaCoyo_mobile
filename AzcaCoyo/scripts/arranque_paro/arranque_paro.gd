0a50609c9fd3755cf12edfa705098b8e5e5119eb
Hector <vwc.hector@gmail.com>
2025-08-19 15:16:39 -0600

    fetch y logica lista de arranque y paro

--- AzcaCoyo/scripts/arranque_paro/arranque_paro.gd
+++ AzcaCoyo/scripts/arranque_paro/arranque_paro.gd
@@ -36,7 +36,7 @@ var data_to_send: Dictionary = {
 };
 
 const HEADERS = ["Content-Type: application/json"]
-var uri_reportes: String = "https://virtualwavecontrol.com.mx/Core24/crud/InsertComando/?idProyecto="
+var uri_reportes: String = "https://virtualwavecontrol.com.mx/Core24/crud/InsertComando?idProyecto="
 	
 func _ready() -> void:
 	GlobalSignals.connect_on_site_row_clicked(_on_site_row_clicked, true)
@@ -56,16 +56,22 @@ func _on_update_app() -> void:
 		lbl_perilla_bomba.text = "REM" if perilla.valor == 1 else "LOC" if perilla.valor == 2 else "OFF"
 
 func _on_button_accion_pressed() -> void:
-	arrancar != arrancar
+	arrancar = !arrancar
 	tr_accion.texture.set("region", boton_accion[arrancar])
+	
+	ejecutar = false
+	tr_ejecutar.texture.set("region", boton_ejecucion[ejecutar])
+	
 	lbl_ejecutando.text = "ARRANCAR" if arrancar else "PARAR"
 
 func _on_button_ejecutar_pressed() -> void:
-	ejecutar != ejecutar
+	ejecutar = !ejecutar
 	tr_ejecutar.texture.set("region", boton_ejecucion[ejecutar])
+	
+	send_command();
 
 func _on_site_row_clicked(_id_proyecto: int, _id_estacion: int):
-	_id_estacion = _id_estacion
+	id_estacion = _id_estacion
 	id_proyecto = _id_proyecto
 	
 	estacion = GlobalData.get_estacion(id_estacion, id_proyecto);
@@ -74,16 +80,22 @@ func _on_site_row_clicked(_id_proyecto: int, _id_estacion: int):
 			id_bomba = _signal.id_signal;
 		if _signal.tipo_signal == TIPO_SIGNAL.Tipo_Signal.PerillaBomba:
 			id_perilla = _signal.id_signal;
+	
+	_on_update_app()
 
 func send_command():
 	lbl_ejecutando.text = "EJECUTANDO\r\nEL COMANDO";
 	data_to_send.Codigo = armar_codigo();
+	data_to_send.idEstacion = id_estacion
 
 	http_request.request(uri_reportes + str(id_proyecto), HEADERS, HTTPClient.METHOD_POST, JSON.stringify(data_to_send))
 	
 func armar_codigo() -> int:
 	return ((id_estacion << 8) | (bomba.ordinal << 4) | ( 1 if arrancar else 2));
 
-func _on_http_request_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
-	if result == http_request.RESULT_SUCCESS:
+func _on_http_request_request_completed(_result: int, _response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
+	if _result == http_request.RESULT_SUCCESS:
 		lbl_ejecutando.text = "COMANDO\r\nEJECUTADO"
+	
+	ejecutar = false
+	tr_ejecutar.texture.set("region", boton_ejecucion[ejecutar])

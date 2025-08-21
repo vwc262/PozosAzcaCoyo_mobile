3bb3f3c98546c503537c188a4b16dabd8eb55e3c
JaxterZepeda <javierzepeda1796@gmail.com>
2025-08-21 09:53:31 -0600

    Tabla

--- AzcaCoyo/scripts/ui/perfil/ui_row_mapa_estacion.gd
+++ AzcaCoyo/scripts/ui/perfil/ui_row_mapa_estacion.gd
@@ -6,6 +6,8 @@ extends Control
 @onready var pressure = %Pressure
 @onready var in_line = %inLine
 
+@onready var site_selected = %SiteSelected
+
 var estacion: Estacion
 var id_Estacion: int
 var id_Proyecto: int
@@ -22,6 +24,8 @@ func _ready():
 	id.text = str(estacion.id_estacion)
 	name_site.text = estacion.nombre
 	GlobalSignals.connect_on_update_app(_on_update_app, true)
+	GlobalSignals.connect_on_site_row_clicked(_on_Site_Click, true)
+	GlobalSignals.connect_on_mini_site_clicked(_reset, true)
 	_on_update_app()
 
 func inicializar_row(_estacion: Estacion):
@@ -37,6 +41,13 @@ func _on_update_app() -> void:
 	pressure.text = str(estacion.signals.get(id_Pressure).get_valor_string())
 	date.text = GlobalUtils.formatear_fecha(estacion.tiempo)
 	in_line.texture.set('region', alarmado_coor[estacion.is_estacion_en_linea()])
+
+func _on_Site_Click(_id_proyecto: int, _id_estacion: int):
+	var select: bool = (_id_estacion == id_Estacion && _id_proyecto == id_Proyecto)
+	site_selected.visible = select
+
+func _reset(_id_estacion: int, _id_proyecto: int):
+	site_selected.visible = _id_proyecto != 0 && _id_estacion == id_Estacion
 
 func _on_button_pressed():
 	GlobalSignals.on_site_row_clicked.emit(id_Proyecto, id_Estacion)
